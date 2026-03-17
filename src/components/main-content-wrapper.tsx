f01b6dc1a8ae4bb767fde4b5093eed0a43741908
EAGLEEARTH <ufukcicek199@gmail.com>
2026-03-17 23:00:37 +0300

    push

--- src/components/main-content-wrapper.tsx
+++ src/components/main-content-wrapper.tsx
@@ -2,7 +2,7 @@
 
 export function MainContentWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex-1 min-w-0 overflow-x-hidden bg-gray-50 dark:bg-gray-950">
+    <main className="flex-1 min-w-0 overflow-x-hidden bg-transparent">
       {children}
     </main>
   );
